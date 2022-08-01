f09f74fcdc29b6ffd4710dfb4acad734043fc0eb
amiralipoor20072007 <98046852+amiralipoor20072007@users.noreply.github.com>
2022-08-01 18:47:24 +0430

    Mirror in Dm ...Fix Bugs...

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -89,5 +89,5 @@ def _clone(message, bot, multi=0):
 def cloneNode(update, context):
     _clone(update.message, context.bot)
 
-clone_handler = CommandHandler(BotCommands.CloneCommand, cloneNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+clone_handler = CommandHandler(BotCommands.CloneCommand, cloneNode, filters=CustomFilters.mebmer_in_group, run_async=True)
 dispatcher.add_handler(clone_handler)
