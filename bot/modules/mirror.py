6ff4afb93e87dc5adad56c87b45629fb5a82f359
amiralipoor20072007 <98046852+amiralipoor20072007@users.noreply.github.com>
2022-08-05 14:24:28 +0430

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -18,7 +18,7 @@ from bot import Interval,AUTHORIZED_CHATS, INDEX_URL,INDEX_BACKUP,IRAN_INDEX_BAC
 from bot.helper.ext_utils.bot_utils import is_url, is_magnet, is_mega_link, is_gdrive_link, get_content_type
 from bot.helper.ext_utils.fs_utils import get_base_name, get_path_size, split_file, clean_download
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException, NotSupportedExtractionArchive
-from bot.helper.mirror_utils.download_utils.aria2_download import add_aria2c_download
+from bot.helper.mirror_utils.download_utils.aria2_download import add_aria2c_download,add_aria2c_download_multi
 from bot.helper.mirror_utils.download_utils.gd_downloader import add_gd_download
 from bot.helper.mirror_utils.download_utils.qbit_downloader import QbDownloader
 from bot.helper.mirror_utils.download_utils.mega_downloader import MegaDownloader
@@ -634,8 +634,7 @@ def _mirror_mulit(bot, message, isZip=False, extract=False, isQbit=False, isLeec
             multi = True
             multiurlspath = f'{DOWNLOAD_DIR}/{random_name}'
             LOGGER.info(f'Thread Aria2c Multi')
-            srun(['cat',multiurlspath])
-            Thread(target=add_aria2c_download, args=(link, f'{DOWNLOAD_DIR}{listener.uid}', listener, name, auth,multiurlspath,multi)).start()
+            Thread(target=add_aria2c_download_multi, args=(link, f'{DOWNLOAD_DIR}{listener.uid}', listener, name, auth,multiurlspath)).start()
 
         if multi > 1:
             sleep(4)
