db2744ff571a8adb5f372e47d0a0841e6169bb02
amiralipoor20072007 <amiralipoor20072007@gmail.com>
2022-08-21 22:02:05 +0430

    Fixing

--- bot/modules/ogatsharei.py
+++ bot/modules/ogatsharei.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from bot.helper.telegram_helper.message_utils import sendMessage
 from bot.helper.telegram_helper.bot_commands import BotCommands
 import requests
-from bot import dispatcher
+from bot import dispatcher , LOGGER
 
 def remove_blanks(List : list):
     while True:
@@ -16,13 +16,14 @@ def remove_blanks(List : list):
 
 
 def oghatsharei(update,context):
+    LOGGER.info('Oghat Sharei')
     page = requests.get('time.ir')
     soup = BeautifulSoup(page,'html.parser')
     forum1 = soup.find_all('div',attrs={'class':'col-sm-6'})[6].text.split('\n')
     forum1 = remove_blanks(forum1)
     forum2 = soup.find_all('div',attrs={'class':'col-sm-6'})[7].text.split('\n')
     forum2 = remove_blanks(forum2)
-    msg = '<b>طلوع شرعی تهران :</b>\n'
+    msg = '<b>اوقات شرعی تهران :</b>\n'
     for i in [0,2,4]:
         msg += f'<b>{forum1[i]}</b> : <b>{forum1[i+1]}</b>'
         msg += f'<b>{forum2[i]}</b> : <b>{forum2[i+1]}</b>'
