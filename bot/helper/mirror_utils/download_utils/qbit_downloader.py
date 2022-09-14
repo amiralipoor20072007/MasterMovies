731af8ee56d25c5c066a52f54ae592cdca1fab30
amiralipoor20072007 <98046852+amiralipoor20072007@users.noreply.github.com>
2022-09-14 23:46:44 +0430

    Update qbit_downloader.py

--- bot/helper/mirror_utils/download_utils/qbit_downloader.py
+++ bot/helper/mirror_utils/download_utils/qbit_downloader.py
@@ -7,7 +7,7 @@ from bot import download_dict, download_dict_lock, BASE_URL, get_client, STOP_DU
 from bot.helper.mirror_utils.status_utils.qbit_download_status import QbDownloadStatus
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, deleteMessage, sendStatusMessage, update_all_messages
-from bot.helper.ext_utils.bot_utils import get_readable_time, setInterval
+from bot.helper.ext_utils.bot_utils import SendSearchMessage, get_readable_time, setInterval
 from bot.helper.ext_utils.fs_utils import clean_unwanted, get_base_name
 from bot.helper.telegram_helper import button_build
 
@@ -125,10 +125,10 @@ class QbDownloader:
                         except:
                             qbname = None
                     if qbname is not None:
-                        qbmsg, button = GoogleDriveHelper().drive_list(qbname, True)
-                        if qbmsg:
+                        search_list, f_name = GoogleDriveHelper().drive_list(qbname, True)
+                        if search_list:
                             self.__onDownloadError("File/Folder is already available in Drive.")
-                            sendMarkup("Here are the search results:", self.__listener.bot, self.__listener.message, button)
+                            SendSearchMessage(self.__listener.message,self.__listener.bot,search_list,f_name)
                     self.__dupChecked = True
             elif tor_info.state == "stalledDL":
                 if not self.__rechecked and 0.99989999999999999 < tor_info.progress < 1:
