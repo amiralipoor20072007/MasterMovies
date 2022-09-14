9904971508ae740d92e7be8303193106ef1d52da
amiralipoor20072007 <98046852+amiralipoor20072007@users.noreply.github.com>
2022-09-14 23:47:27 +0430

    Update telegram_downloader.py

--- bot/helper/mirror_utils/download_utils/telegram_downloader.py
+++ bot/helper/mirror_utils/download_utils/telegram_downloader.py
@@ -3,6 +3,7 @@ from time import sleep, time
 from threading import RLock, Lock
 
 from bot import LOGGER, download_dict, download_dict_lock, STOP_DUPLICATE, app,Premuim_app
+from bot.helper.ext_utils.bot_utils import SendSearchMessage
 from ..status_utils.telegram_download_status import TelegramDownloadStatus
 from bot.helper.telegram_helper.message_utils import sendMarkup, sendMessage, sendStatusMessage
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
@@ -141,10 +142,10 @@ class TelegramDownloadHelper:
                     size = media.file_size
                     if STOP_DUPLICATE and not self.__listener.isLeech:
                         LOGGER.info('Checking File/Folder if already in Drive...')
-                        smsg, button = GoogleDriveHelper().drive_list(name, True, True)
-                        if smsg:
-                            msg = "File/Folder is already available in Drive.\nHere are the search results:"
-                            return sendMarkup(msg, self.__listener.bot, self.__listener.message, button)
+                        search_list, f_name = GoogleDriveHelper().drive_list(name, True, True)
+                        if search_list:
+                            SendSearchMessage(self.__listener.message,self.__listener.bot,search_list,f_name)
+                            return
                     self.__onDownloadStart(name, size, media.file_unique_id)
                     LOGGER.info(f'Downloading Telegram file with id: {media.file_unique_id}')
                     self.__download(_dmsg, path)
